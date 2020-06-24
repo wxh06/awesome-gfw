2a13335c0642e25dc16eb60ba0a6498e384ea197
汪心禾 <wangxinhe06@gmail.com>
2020-06-24 09:32:33 +0800

    Fix arrow-body-style

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const languages = [['en-US', 'zh-CN'], ['en', 'zh']];
 
 if (window.navigator.languages) {
-  languages.some((al) => {
-    return window.navigator.languages.some((l) => {
-      if (al.includes(l)) {
-        window.location.replace(`${l}/`);
-        return true;
-      }
-      return false;
-    });
-  });
+  languages.some((al) => window.navigator.languages.some((l) => {
+    if (al.includes(l)) {
+      window.location.replace(`${l}/`);
+      return true;
+    }
+    return false;
+  }));
 }
